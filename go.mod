aeffe56f0305e758113d3bee7130fa2b24d464d3
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-09-05 14:04:07 +0800

    Initialize project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Autodeploy
+
+go 1.21.0
+
+require github.com/google/uuid v1.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var port string = "8000"
+
+type Payload struct {
+	Repository struct {
+		CloneURL      string `json:"clone_url"`
+		DefaultBranch string `json:"default_branch"`
+	} `json:"repository"`
+}
+
+func main() {
+	http.HandleFunc("/githubwebhook", payloadHandler)
+
+	fmt.Println("Server listening on port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func payloadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		var payload Payload
+		err = json.Unmarshal(body, &payload)
+		if err != nil {
+			http.Error(w, "Failed to parse JSON body", http.StatusBadRequest)
+			return
+		}
+
+		var UUID string = uuid.New().String()
+
+		fmt.Println("Clone URL:", payload.Repository.CloneURL)
+		fmt.Println("Default Branch:", payload.Repository.DefaultBranch)
+		fmt.Println("UUID:", UUID)
+
+		os.Setenv("__GIT_REPO__", payload.Repository.CloneURL)
+		os.Setenv("__GIT_BRANCH__", payload.Repository.DefaultBranch)
+		os.Setenv("__UUID__", UUID)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}

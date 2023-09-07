a7c77387dc219a646bc591127153bf242b813f9e
OnCloud <anonymousaaaa41414141@gmail.com>
2023-09-08 01:36:19 +0800

    Update build script

--- go_build.sh
+++ go_build.sh
@@ -5,7 +5,6 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
-ORANGE='\033[0;33m'
 MAGENTA='\033[0;35m'
 NC='\033[0m' # No Color
 
@@ -14,6 +13,9 @@ CHECKMARK='\xE2\x9C\x94'
 ROCKET='\xF0\x9F\x9A\x80'
 CROSSMARK='\xE2\x9C\x98'
 HOURGLASS='\xE2\x8C\x9B'
+
+# Define docker builder filename
+docker_builder="docker_builder.sh"
 
 # Check if Go module is enabled
 if go env GOMOD &> /dev/null; then
@@ -49,11 +51,60 @@ if [ $# -gt 0 ]; then
             echo -e "  ${RED}${CROSSMARK} File or directory not found: $arg${NC}"
         fi
     done
-    echo ""
-    echo -e "${GREEN}All operations completed successfully!${NC}"
 else
-    echo -e "${ORANGE}Warning: No dependencies provided.${NC}"
+    echo -e "${YELLOW}Warning: No dependencies provided.${NC}"
 fi
 
+# Check and create necessary dependencies
+echo ""
+if [ ! -f "_production/$docker_builder" ] || [ ! -d "_production/Dockerfiles" ] || [ ! -d "_production/logs" ]; then
+    echo -e "${MAGENTA}Creating necessary dependencies...${NC}"
+fi
+if [ ! -f "_production/$docker_builder" ]; then
+    cp "$docker_builder" "_production/"
+    echo -e "  ${MAGENTA}${CHECKMARK} $docker_builder${NC}"
+fi
+if [ ! -d "_production/Dockerfiles" ]; then
+    mkdir -p "_production/Dockerfiles"
+    echo -e "  ${MAGENTA}${CHECKMARK} Dockerfiles${NC}"
+fi
+if [ ! -d "_production/logs" ]; then
+    mkdir -p "_production/logs"
+    echo -e "  ${MAGENTA}${CHECKMARK} logs${NC}"
+fi
+
+# Gain the execute permission
+chmod +x ./_production/$executable
+chmod +x ./_production/$docker_builder
+
+# Create run file
+cat << EOF > _production/run_in_background.sh
+#!/bin/bash
+nohup ./SpiderTrigger &
+echo \$! > nohup_pid
+EOF
+
+# Create kill file
+cat << EOF > _production/kill_run_in_background.sh
+#!/bin/bash
+if [ -f "nohup_pid" ]; then
+    pid=\$(cat nohup_pid)
+    if [ -n "\$pid" ]; then
+        echo "Killing process \$pid..."
+        kill \$pid
+        rm nohup_pid
+        echo "Process killed."
+    else
+        echo "No process ID found in nohup_pid file."
+    fi
+else
+    echo "nohup_pid file not found."
+fi
+EOF
+
+echo ""
+echo -e "${GREEN}All operations completed successfully!${NC}"
 echo ""
 echo -e "${ROCKET} You can now use ${BLUE}./_production/$executable${NC} to execute your program."
+echo -e "${ROCKET}${YELLOW} To run your program in the background, use ${BLUE}./_production/run_in_background.sh${YELLOW}.${NC}"
+echo -e "${ROCKET}${YELLOW} To stop the program running in the background, use ${BLUE}./_production/kill_run_in_background.sh${YELLOW}.${NC}"
